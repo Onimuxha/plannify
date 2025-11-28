180e8022aaa6504d4a1e96786111fcb3bbaa3a02
Onimuxha <cheathekm@gmail.com>
2025-11-28 13:50:25 +0700

    update img

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,8 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        {/* <meta name="apple-mobile-web-app-capable" content="yes" /> */}
+        <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body className={`${outfit.variable} font-sans antialiased`}>
